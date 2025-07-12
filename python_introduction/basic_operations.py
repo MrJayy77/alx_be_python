78259ef4df07e4fb77fe7ef014d4872fc990a1fb
Abraham James <abrahamkwesijames@gmail.com>
2025-07-12 13:12:17 +0100

    python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Define operations
+number1 = 10
+number2 = 5 
+
+# Step 2: Do the math
+sum_result = number1 + number2        # Addition
+difference = number1 - number2        # Subtraction
+product = number1 * number2           # Multiplication
+
+# Step 3: Show the answers
+print("Addition of", number1, "and", number2, "is", sum_result)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
