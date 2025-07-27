0177293f92caa5495349ba5b9d92a977c219eeb7
Abraham James <abrahamkwesijames@gmail.com>
2025-07-27 16:57:57 +0100

    Yayy

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,24 +1,17 @@
-def perform_operation():
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = (input("Enter the operation (+, -, *, /): "))
+# arithmetic_operations.py
 
+def perform_operation(num1, num2, operation):
     match operation:
-        case '+':
-            result = num1 + num2
-        case '-':
-            result = num1 - num2
-        case '*':
-            result = num1 * num2
-        case '/':
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
             if num2 != 0:
-                result = num1 / num2
+                return num1 / num2
             else:
-                print("Error: Division by zero is not allowed.")
-                return
+                return "Error: Division by zero is not allowed."
         case _:
-            print("Invalid operation.")
-            return
-        
-    print(f"Result: {result}")
-perform_operation()
+            return "Invalid operation."
