62a109e3cb327d59e620993b3d857ec7ef50c0e0
Abraham James <abrahamkwesijames@gmail.com>
2025-08-03 16:46:57 +0100

    Yayy

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,21 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.__account_balance = account_balance  # encapsulated attribute
+        self.__account_balance = float(account_balance)  # Private variable for encapsulation
 
     def deposit(self, amount):
-        self.__account_balance += amount
-        return f"Deposited {amount}. Current balance: {self.__account_balance}"
+        self.__account_balance += float(amount)
+        return f"Deposited: ${float(amount):.2f}"
 
     def withdraw(self, amount):
+        amount = float(amount)
         if amount <= self.__account_balance:
             self.__account_balance -= amount
-            return f"Withdrew {amount}. Current balance: {self.__account_balance}"
+            return f"Withdrew: ${amount:.2f}"
         else:
-            return "Insufficient funds"
+            return "Insufficient funds."
 
     def display_balance(self):
-        return f"Current Balance: {self.__account_balance}"
+        return f"Current Balance: ${self.__account_balance:.2f}"
+
 
 
