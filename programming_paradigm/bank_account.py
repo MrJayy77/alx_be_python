14db3ac23f1563d4f325be40ecc49cda794251ee
Abraham Kwesi James <abrahamkwesijames@gmail.com>
2025-08-03 16:36:09 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,8 +3,9 @@ class BankAccount:
         self.account_balance = account_balance 
 
     def deposit(self, amount):
-        self.account_balance += amount
-        return self.account_balance
+    self.account_balance += amount
+    return f"Deposited {amount}. New balance is: {self.account_balance}"
+
     
     def withdraw(self, amount):
         if amount <= self.account_balance:
