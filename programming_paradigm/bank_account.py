bae223c345f1e1454f2c122e13c2bdfaa8a7a70c
Abraham James <abrahamkwesijames@gmail.com>
2025-08-03 16:26:26 +0100

    Yayy

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,22 +3,8 @@ class BankAccount:
         self.__account_balance = account_balance  # encapsulated attribute
 
     def deposit(self, amount):
-<<<<<<< HEAD
         self.__account_balance += amount
         return f"Deposited {amount}. New balance is: {self.__account_balance}"
-=======
-    self.account_balance += amount
-    return f"Deposited {amount}. New balance is: {self.account_balance}"
-
-    
-   def withdraw(self, amount):
-    if amount <= self.account_balance:
-        self.account_balance -= amount
-        return f"Withdrew {amount}. New balance is: {self.account_balance}"
-    else:
-        return "Insufficient funds"
-
->>>>>>> 844798b908568f5244c74887a3744f7d0ce78b95
 
     def withdraw(self, amount):
         if amount <= self.__account_balance:
